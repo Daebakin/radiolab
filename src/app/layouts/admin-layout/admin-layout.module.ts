6d69678ff20cbea23beee7a54bcb58c3ef8339ac
Jeffrey Aboh <jefferyaboh@gmail.com>
2022-10-06 15:22:08 +0100

    Updates - Oct 6 2022

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -5,22 +5,14 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 
 import { AdminLayoutRoutes } from "./admin-layout.routing";
-import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
-import { IconsComponent } from "../../pages/icons/icons.component";
-import { MapComponent } from "../../pages/map/map.component";
-import { NotificationsComponent } from "../../pages/notifications/notifications.component";
-import { UserComponent } from "../../pages/user/user.component";
-import { TablesComponent } from "../../pages/tables/tables.component";
 import { EditLoanLimitDialog } from "src/app/pages/platform/trovilo/edit-users/edit-users.component";
-import { TypographyComponent } from "../../pages/typography/typography.component";
-// import { RtlComponent } from "../../pages/rtl/rtl.component";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import {MatChipsModule} from '@angular/material/chips';
-import {MatAutocompleteModule} from '@angular/material/autocomplete'; 
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatDialogModule} from '@angular/material/dialog'; 
+import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
 import {MatExpansionModule} from '@angular/material/expansion';
 
@@ -40,15 +32,7 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatAutocompleteModule,
   ],
   declarations: [
-    DashboardComponent,
-    UserComponent,
-    TablesComponent,
     EditLoanLimitDialog,
-    IconsComponent,
-    TypographyComponent,
-    NotificationsComponent,
-    MapComponent,
-    // RtlComponent
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -1,14 +1,5 @@
 import { Routes } from "@angular/router";
 
-//import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
-import { IconsComponent } from "../../pages/icons/icons.component";
-import { MapComponent } from "../../pages/map/map.component";
-import { NotificationsComponent } from "../../pages/notifications/notifications.component";
-import { UserComponent } from "../../pages/user/user.component";
-import { TablesComponent } from "../../pages/tables/tables.component";
-import { TypographyComponent } from "../../pages/typography/typography.component";
-// import { RtlComponent } from "../../pages/rtl/rtl.component";
-
 // Added Routes
 import { UsersComponent } from "src/app/pages/platform/trovilo/users/users.component";
 import { DashboardComponent } from "src/app/pages/platform/trovilo/dashboard/dashboard.component";
@@ -20,13 +11,6 @@ import { ManageLoansComponent } from "src/app/pages/platform/trovilo/manage-loan
 import { EditLoansComponent } from "src/app/pages/platform/trovilo/edit-loans/edit-loans.component";
 
 export const AdminLayoutRoutes: Routes = [
-  { path: "dashboard", component: DashboardComponent },
-  { path: "icons", component: IconsComponent },
-  { path: "maps", component: MapComponent },
-  { path: "notifications", component: NotificationsComponent },
-  { path: "user", component: UserComponent },
-  { path: "tables", component: TablesComponent },
-  { path: "typography", component: TypographyComponent },
 
   //Added Routes
 
